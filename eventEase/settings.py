2fd17bccd0dbbb36df205d88d8b251917613773d
Joesph Afonne <josef.afonne@gmail.com>
2025-01-23 15:13:22 +0000

    Testing cloud file upload by removing DEFAULT FILE STORAGE SETTING

--- eventEase/settings.py
+++ eventEase/settings.py
@@ -180,4 +180,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEFAULT_FILE_STORAGE = os.environ.get('CLOUDINARY_URL')
+#DEFAULT_FILE_STORAGE = os.environ.get('CLOUDINARY_URL')
