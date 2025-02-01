f6afad750ea9f036db97f72f37197f0e5ab821af
Joesph Afonne <josef.afonne@gmail.com>
2025-02-01 06:32:26 +0000

    Add about us page

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")        
+        print(f"Received message from {name} ({email}): {message}")
+
+        messages.success(request, "Your message has been sent successfully!")
+        return redirect('contact')         
     return render(request, 'contact/contact.html')
